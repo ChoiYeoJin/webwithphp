dd02891a6fd56b49a32fc07e89028e812bed69e9
yeojin <sonata7531@gmail.com>
2023-07-31 00:57:43 +0900

    signin update

--- signin.php
+++ signin.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-<?php
-//나중에 로그인 하면
-$user_id = 1;
-$conn = mysqli_connect("localhost", "root", 1111);
-mysqli_select_db($conn, "study_project");
-$class_res = mysqli_query($conn, "SELECT * FROM class WHERE class_id IN ( SELECT class_id FROM user_class WHERE user_id = " . $user_id . " )");
-
-
-?>
-
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="http://localhost/webwithphp/css/style.css">
@@ -18,20 +8,20 @@ $class_res = mysqli_query($conn, "SELECT * FROM class WHERE class_id IN ( SELECT
 
 <body id="target">
     <header>
-        <img src="./img/icon_19.png" alt="공부">
+        <img src="./img/pngwing.com.png" alt="공부" width="200px">
         <h1>Sign in</a></h1>
     </header>
 
     <article>
-        <form action="process.php" method="POST">
+        <form action="signin_process.php" method="POST">
             <p>
-                name : <input type = "text" name = "user_name">                
+                name : <input type="text" name="user_name">
             </p>
             <p>
-                email : <input type = "text" name = "email">                
+                email : <input type="text" name="email">
             </p>
             <p>
-                password : <input type = "text" name = "password">
+                password : <input type="text" name="password">
             </p>
             <input type="submit" name="name">
         </form>
